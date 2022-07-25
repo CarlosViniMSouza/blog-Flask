380fbe56e07af0f505f4adcca5113909fd49c00a
Carlos Vinicius <vinicius.souza5530@gmail.com>
2022-07-25 00:28:48 +0000

    css -> added

--- app/routes.py
+++ app/routes.py
@@ -5,4 +5,27 @@ from app import app
 @app.route("/index")
 def index():
     user = "CarlosViniMSouza"
-    return render_template("index.html", title="blog Flask", user=user)
+    posts = [
+        {
+            "username": "Carlos",
+            "title": "Intro the Flask Framework",
+            "subtitle": "A simple tutorial about Flask",
+            "posted": True
+        },
+
+        {
+            "username": "Vinicius",
+            "title": "Intro the Django Ecossystem",
+            "subtitle": "A tutorial about Django and DjangoRest",
+            "posted": False
+        },
+
+        {
+            "username": "De Souza",
+            "title": "Intro the FastAPI",
+            "subtitle": "A simple tutorial about RestAPIs with FastAPI",
+            "posted": True
+        },
+    ]
+
+    return render_template("index.html", title="blog Flask", user=user, posts=posts)
--- autogit.sh
+++ autogit.sh
@@ -1,4 +1,4 @@
 git add .
-git commit -m "templates -> added"
+git commit -m "css -> added"
 git push -u origin main
 git status
